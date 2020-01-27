ca7c77cc900040fff82159bb3d93be866e3937c1
Vik Kudesia <vvkudesia@gmail.com>
2020-01-26 19:55:49 -0500

    ADD AreaCalc drill but it's busted and won't compile due to failure to abstract interface

--- classes/AreaCalc/AreaCalc_drill.java
+++ classes/AreaCalc/AreaCalc_drill.java
@@ -6,17 +6,17 @@ interface AreaCalStyle // Using a blueprinting method to instruct classes on wha
 class AreaOfCircles implements AreaCalStyle
 {
     double area;
-    plublic void circle(double r)
+    public void circle(double r)
     {
         area= (22*r*r)/7;
     }
     public static void main(String args[])
     {
-        AreaOfCircle x;
+        AreaOfCircles x;
         Scanner s= new Scanner(System.in);
         System.out.println("Type in the radius of a circle:");
         double rad= s.nextDouble();
-        x= new AreaOfCircle();
+        x=new AreaOfCircles();
         x.circle(rad);
         System.out.println("The area of the circle is: " + x.area);
     }
