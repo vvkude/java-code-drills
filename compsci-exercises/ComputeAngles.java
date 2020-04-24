5113958bac024477e3bc177ca2b34670793c8f64
Vik Kudesia <vvkudesia@gmail.com>
2020-04-23 21:24:10 -0400

    ADD initial draft of a program to compute the angles of a triangle

---
+++ compsci-exercises/ComputeAngles.java
@@ -0,0 +1,39 @@
+// Assignment: Compute the angles of a triangle given user input for the points
+
+public class ComputeAngles {
+	public static void main(String[] args) {
+
+		// TODO change this to prompt the user for three points
+		double x1 = 23.23;
+		double y1 = 12.12;
+		double x2 = 54.24;
+		double y2 = 56.56;
+		double x3 = 78.00;
+		double y3 = 102.12;
+
+		// TODO should use Triangle Inequality Theorem to check that the sum of any 2
+		// sides of a triangle must be greater than the measure of the third side
+
+		// Compute the length of the sides first
+		double a = Math.sqrt((x2 - x3) * (x2 - x3) 
+			+ (y2 - y3) * (y2 - y3));
+		double b = Math.sqrt((x1 - x3) * (x1 - x3)
+			+ (y1 - y3) * (y1 - y3));
+		double c = Math.sqrt((x1 - x2) * (x1 - x2)
+			+ (y1 - y2) * (y1 - y2));
+
+		// Use the arccosine(x) to compute the angles of the triangle
+		double A = Math.toDegrees(Math.acos((a * a - b * b - c * c)
+			/ (-2 * b * c)));
+		double B = Math.toDegrees(Math.acos((b * b - a * a - c * c)
+			/ (-2 * a * c)));
+		double C = Math.toDegrees(Math.acos((c * c - b * b - a * a)
+			/ (-2 * a * b)));
+
+		// Output results to the user for review
+		System.out.println("The three angles are " +
+			Math.round(A * 100) / 100.0 + " " + 
+			Math.round(B * 100) / 100.0 + " " +
+			Math.round(C * 100) / 100.0);
+	}
+}
