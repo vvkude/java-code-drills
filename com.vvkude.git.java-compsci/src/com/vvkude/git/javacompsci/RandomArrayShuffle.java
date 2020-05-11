5960cccacfe6df72e7df6b977ea029a3316b3002
Vik Kudesia <vvkudesia@gmail.com>
2020-05-11 12:27:36 -0400

    ADD snippet that moves array elements to the left by 1

---
+++ com.vvkude.git.java-compsci/src/com/vvkude/git/javacompsci/RandomArrayShuffle.java
@@ -0,0 +1,21 @@
+package com.vvkude.git.javacompsci;
+
+public class RandomArrayShuffle {
+
+	public static void main(String[] args) {
+		double[] myArray = {1.0, 2.0, 3.0, 4.0, 5.0};
+		
+		// Display the initial array
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println(myArray[i] + " ");
+		}
+		
+		// Move all array elements to the left by one
+		
+		// Move the array's first element to the end of the array
+		
+		// Display the new array
+
+	}
+
+}
