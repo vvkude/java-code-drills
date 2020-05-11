967158cea709712b26e31828eba2df40d630a285
Vik Kudesia <vvkudesia@gmail.com>
2020-05-11 12:37:53 -0400

    UPDATE array elements now shift to the left by 1 element

--- com.vvkude.git.java-compsci/src/com/vvkude/git/javacompsci/RandomArrayShuffle.java
+++ com.vvkude.git.java-compsci/src/com/vvkude/git/javacompsci/RandomArrayShuffle.java
@@ -10,11 +10,20 @@ public class RandomArrayShuffle {
 			System.out.println(myArray[i] + " ");
 		}
 		
+		double temp = myArray[0]; // to hold the first array element
+		
 		// Move all array elements to the left by one
+		for (int i = 1; i < myArray.length; i++) {
+			myArray[i - 1] = myArray[i];
+		}
 		
 		// Move the array's first element to the end of the array
+		myArray[myArray.length - 1] = temp;
 		
 		// Display the new array
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println(myArray[i] + " ");
+		}
 
 	}
 
