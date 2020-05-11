c0204f08393a200732dadded94df5c382eb82e4c
Vik Kudesia <vvkudesia@gmail.com>
2020-05-11 13:50:34 -0400

    ADD stubbed deck of cards and random draw program

---
+++ com.vvkude.git.java-compsci/src/com/vvkude/git/javacompsci/DeckOfCards.java
@@ -0,0 +1,28 @@
+package com.vvkude.git.javacompsci;
+
+/*
+ * Problem: Write a program that will randomly draw 4 cards from a standard deck of cards
+ * 
+ * Solution: Store the cards in an array along with suits and ranks
+ * Integer division should yield the suit
+ * Modulo operator should yield the rank
+ */
+
+public class DeckOfCards {
+
+	public static void main(String[] args) {
+		// TODO Create an array of 52 elements for the deck
+		
+		// TODO Create an array of strings for suits
+		
+		// TODO Create an array of strings for ranks
+		
+		// TODO Assign values to all the cards in the deck
+		
+		// TODO Randomly shuffle the deck of cards
+		
+		// TODO Display the first four cards to the user
+
+	}
+
+}
